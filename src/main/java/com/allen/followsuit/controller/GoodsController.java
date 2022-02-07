f4b8470cd982d739e827c7cad4caaaddb828c574
Allen <610314595@qq.com>
2022-02-07 22:17:58 +0800

    change bug

--- src/main/java/com/allen/followsuit/controller/GoodsController.java
+++ src/main/java/com/allen/followsuit/controller/GoodsController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.allen.followsuit.controller;
 
 import com.allen.followsuit.common.Result;
@@ -66,72 +66,3 @@ public class GoodsController {
         return Result.success(goodsService.findPage(pageNum, pageSize, name));
     }
 }
-=======
-package com.allen.followsuit.controller;
-
-import com.allen.followsuit.common.Result;
-import com.allen.followsuit.entity.Goods;
-import com.allen.followsuit.service.GoodsService;
-import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
-import java.util.List;
-
-
-@RestController
-@RequestMapping("/data_goods")
-public class GoodsController {
-
-    @Resource
-    private GoodsService goodsService;
-
-    @PostMapping
-    public Result add(@RequestBody Goods goods) {
-        goodsService.save(goods);
-        return Result.success();
-    }
-
-    @PutMapping
-    public Result update(@RequestBody Goods goods) {
-        goodsService.save(goods);
-        return Result.success();
-    }
-
-//    @DeleteMapping("/{id}")
-//    public Result delete(@PathVariable("id") Long id){
-//        goodsService.del(id);
-//        return Result.success();
-//    }
-
-    @GetMapping("/delete/{goodsid}")
-    public Result delete(@PathVariable("goodsid") Long goodsid) {
-        goodsService.del(goodsid);
-        return Result.success();
-    }
-
-
-    @GetMapping
-    public Result<List<Goods>> findAllgoods() {
-        return Result.success(goodsService.findAllgoods());
-    }
-
-    @GetMapping("/test")
-    public Result<List<Goods>> testfindid() {
-        return Result.success(goodsService.testid());
-    }
-
-    @DeleteMapping("/testdel/{name}")
-    public Result testdel(@PathVariable("name") String name) {
-        goodsService.testdel(name);
-        return Result.success();
-    }
-
-    @GetMapping("/page")
-    public Result<Page<Goods>> findPage(@RequestParam(defaultValue = "1") Integer pageNum,
-                                        @RequestParam(defaultValue = "10") Integer pageSize,
-                                        @RequestParam(required = false) String name) {
-        return Result.success(goodsService.findPage(pageNum, pageSize, name));
-    }
-}
->>>>>>> b97d8588d5aabe3fae76e28b18f008c6fab54745
